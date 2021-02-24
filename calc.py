ec2ee34555d2a81a5d6dbafc23c817d2a07a83db
nidemidovich <demidovicnikita1@gmail.com>
2021-02-24 12:11:35 +0300

    Rewrote to class. Added loop to calculate many smiles.

---
+++ calc.py
@@ -0,0 +1,111 @@
+import os
+import sys
+from subprocess import call
+
+from ase.calculators.gamess_us import GAMESSUS
+from ase.io import read
+from openbabel import pybel
+
+class Job:
+    
+    # Optimize calc
+    opt_calc = GAMESSUS(basis=dict(gbasis='sto', ngauss=3), 
+                        contrl=dict(scftyp='rhf', runtyp='optimize'),
+                        statpt=dict(opttol=0.0001, nstep=20))
+
+    # Energy calc
+    energy_calc = GAMESSUS(basis=dict(gbasis='sto', ngauss=3),
+                           contrl=dict(scftyp='rhf', runtyp='energy'))
+    
+    @staticmethod
+    def run_gamess(name, input_file, version, 
+                   ncpus, output_name, input_directory):
+        try:
+            os.remove(os.path.join('restart', name+'.dat'))
+        except:
+            pass
+	
+        rungms = ''
+        if sys.platform == 'win32':
+            rungms = 'rungms.bat'
+        else:
+            rungms = 'rungms'
+		
+        call([rungms, input_file, version, str(ncpus), output_name], 
+             shell=True)
+        os.rename(output_name, os.path.join(input_directory, output_name))
+        print('\n\n')
+    
+    def __init__(self, i, smiles, ncpus, version, path_to_gamess):
+        self.smiles = smiles.rstrip()
+        self.ncpus = ncpus
+        self.version = version
+        self.path_to_gamess = path_to_gamess
+        self.name = str(i)
+        
+    def smi_to_mol(self):
+        smiles = pybel.readstring('smi', self.smiles)
+        smiles.make3D()
+        smiles.write(format='mol', filename=self.name+'.mol', overwrite=True)
+        
+    def create_inp_file(self):
+        mol = read(self.name+'.mol')
+        Job.opt_calc.write_input(atoms=mol)
+        self.mol_for_opt_filename = self.name + '_for_opt.inp'
+        os.rename('gamess_us.inp', self.mol_for_opt_filename)
+        self.mol_for_opt_name = os.path.splitext(self.mol_for_opt_filename)[0]
+    
+    def geometry_optimization(self):
+        self.output_name = self.name+'_opt.gamout'
+        input_directory = os.getcwd()
+        os.rename(self.mol_for_opt_filename,
+                  os.path.join(self.path_to_gamess, self.mol_for_opt_filename))
+        os.chdir(self.path_to_gamess)
+        print(self.mol_for_opt_filename, os.getcwd())
+
+        Job.run_gamess(name=self.mol_for_opt_name,
+                       input_file=self.mol_for_opt_filename,
+                       version=self.version,
+                       ncpus=self.ncpus,
+                       output_name=self.output_name,
+                       input_directory=input_directory)
+        
+        os.remove(self.mol_for_opt_filename)
+        os.chdir(input_directory)
+        
+    def get_optimized_coords(self):
+        mol = next(pybel.readfile('gamout', self.output_name))
+        mol.write(format='mol', filename='coords_opt.mol', overwrite=True)
+        
+    def create_inp_file_for_nrg_calc(self):
+        mol = read('coords_opt.mol')
+        Job.energy_calc.write_input(atoms=mol)
+        self.mol_for_nrg_filename = self.name + '_for_nrg.inp'
+        os.rename('gamess_us.inp', self.mol_for_nrg_filename)
+        self.mol_for_nrg_name = os.path.splitext(self.mol_for_nrg_filename)[0]
+        os.remove('coords_opt.mol')
+        
+    def nrg_calc(self):
+        self.output_name = self.name+'_nrg.gamout'
+        input_directory = os.getcwd()
+        os.rename(self.mol_for_nrg_filename, 
+                  os.path.join(self.path_to_gamess, self.mol_for_nrg_filename))
+        os.chdir(self.path_to_gamess)
+
+        Job.run_gamess(name=self.mol_for_nrg_name,
+                   input_file=self.mol_for_nrg_filename,
+                   version=self.version,
+                   ncpus=self.ncpus,
+                   output_name=self.output_name,
+                   input_directory=input_directory)
+
+        os.remove(self.mol_for_nrg_filename)
+        os.chdir(input_directory)
+        
+    def run(self):
+        self.smi_to_mol()
+        self.create_inp_file()
+        self.geometry_optimization()
+        self.get_optimized_coords()
+        self.create_inp_file_for_nrg_calc()
+        self.nrg_calc()
--- script.py
+++ script.py
@@ -1,44 +1,11 @@
-# -*- coding: utf-8 -*-
-import json
-import os
 import sys
-from subprocess import call
+import json
 
-from ase.calculators.gamess_us import GAMESSUS
-from ase.io import read
-from ase.io import write
-from openbabel import pybel
-
-# Optimize calc
-opt_calc = GAMESSUS(basis=dict(gbasis='sto', ngauss=3), 
-					contrl=dict(scftyp='rhf', runtyp='optimize'),
-					statpt=dict(opttol=0.0001, nstep=20))
-
-# Energy calc
-energy_calc = GAMESSUS(basis=dict(gbasis='sto', ngauss=3),
-					   contrl=dict(scftyp='rhf', runtyp='energy'))
-
-def run_gamess(name, input_file, version, ncpus, output_name, input_directory):
-	try:
-		os.remove(os.path.join('restart', name+'.dat'))
-	except:
-		pass
-	
-	rungms = ''
-	if sys.platform == 'win32':
-		rungms = 'rungms.bat'
-	else:
-		rungms = 'rungms'
-		
-	call([rungms, input_file, version, str(ncpus), output_name], 
-		 shell=True)
-	os.rename(output_name, os.path.join(input_directory, output_name))
-	print('\n\n')
+from calc import Job
 
 # Get config
 with open('config.json') as f:
 	config = json.load(f)
-print(config)
 
 if sys.platform == 'win32':
 	path_to_gamess = config['path']['Windows']
@@ -59,62 +26,11 @@ try:
 	ncpus = sys.argv[2] 
 except:
 	ncpus = 1
-	
-name = input_file.split('.')[0]
 
-# smi to mol
-smiles = next(pybel.readfile('smi', input_file))
-smiles.make3D()
-smiles.write(format='mol', filename=name+'.mol', overwrite=True)
-
-# Create inp file for opt
-mol = read(name+'.mol')
-opt_calc.write_input(atoms=mol)
-mol_for_opt_filename = name + '_for_opt.inp'
-os.rename('gamess_us.inp', mol_for_opt_filename)
-mol_for_opt_name = mol_for_opt_filename.split('.')[0]
-
-# Geometry optimization
-output_name = name+'_opt.gamout'
-input_directory = os.getcwd()
-os.rename(mol_for_opt_filename,
-		  os.path.join(path_to_gamess, mol_for_opt_filename))
-os.chdir(path_to_gamess)
-
-run_gamess(name=mol_for_opt_name,
-		   input_file=mol_for_opt_filename,
-		   version=version,
-		   ncpus=ncpus,
-		   output_name=output_name,
-		   input_directory=input_directory)
-
-os.remove(mol_for_opt_filename)
-os.chdir(input_directory)
-
-# Get optimized coordinates
-mol = next(pybel.readfile('gamout', output_name))
-mol.write(format='mol', filename='coords_opt.mol', overwrite=True)
-
-#Create inp file for energy calculation
-mol = read('coords_opt.mol')
-energy_calc.write_input(atoms=mol)
-mol_for_nrg_filename = name + '_for_nrg.inp'
-os.rename('gamess_us.inp', mol_for_nrg_filename)
-mol_for_nrg_name = mol_for_nrg_filename.split('.')[0]
-
-#Energy calculation
-output_name = name+'_nrg.gamout'
-input_directory = os.getcwd()
-os.rename(mol_for_nrg_filename, 
-		  os.path.join(path_to_gamess, mol_for_nrg_filename))
-os.chdir(path_to_gamess)
-
-run_gamess(name=mol_for_nrg_name,
-		   input_file=mol_for_nrg_filename,
-		   version=version,
-		   ncpus=ncpus,
-		   output_name=output_name,
-		   input_directory=input_directory)
-
-os.remove(mol_for_nrg_filename)
-os.chdir(input_directory)
+with open(input_file) as f:
+    i = 0
+    for smiles in f:
+        i += 1
+        print(smiles)
+        job = Job(i, smiles, ncpus, version, path_to_gamess)
+        job.run()
